edd870e98c3c99c4775b798a6fee7dfc76401b3f
impROS <impROS@hotmail.com>
2018-06-07 07:46:39 +0300

    Update arac_lamba.ino
    Butona algılama kararsızlığı düzeltildi.

--- arac_lamba.ino
+++ arac_lamba.ino
@@ -24,29 +24,33 @@ int butonSolSinyal = 18;
 int butonFar = 19;
 
 int sayac = 0;
+unsigned long simdikiZaman;
+unsigned long basmaZamani;
+unsigned long fark;
+int dortluSayac = 0;
 void setup() {
   //Pin Hazirlama
   pinMode(ledDortlu, OUTPUT);
   pinMode(ledSag, OUTPUT);
   pinMode(ledSol, OUTPUT);
   pinMode(ledFar, OUTPUT);
- // pinMode(besVolt,OUTPUT);
+  // pinMode(besVolt,OUTPUT);
 
   pinMode(buzzer, OUTPUT);
 
   Serial.begin(9600);
 
   //Kesmeler
-  attachInterrupt(digitalPinToInterrupt(butonSagSinyal), sagSinyalOku, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(butonDortlu), dortluOku, CHANGE );
-  attachInterrupt(digitalPinToInterrupt(butonSolSinyal), solSinyalOku, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(butonFar), farOku, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(butonSagSinyal), sagSinyalOku, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butonDortlu), dortluOku, FALLING );
+  attachInterrupt(digitalPinToInterrupt(butonSolSinyal), solSinyalOku, FALLING);
+  attachInterrupt(digitalPinToInterrupt(butonFar), farOku, FALLING);
   //  for(int i =8;i<14;i++){
   //    pinMode(i,OUTPUT);
   //    digitalWrite(i,HIGH);
   //    }
 
- // digitalWrite(besVolt,HIGH);
+  // digitalWrite(besVolt,HIGH);
 
 }
 
@@ -60,53 +64,119 @@ void loop() {
 
 }
 void sagSinyalOku() {
-  sayac++;
-  if (sayac % 2 == 0) {
-    Serial.println("sagSinyal");
-    sagDurum = !sagDurum;
-    sayac = 0;
+  if (digitalRead(butonSagSinyal) == LOW) {
+    sayac++;
+    if (sayac % 2 == 0) {
+      sayac = 0;
+      dortluSayac++;
+      if (dortluSayac == 1) {
+        basmaZamani = millis();
+        // Serial.println(" ilk Sag Rising : ");
+        // Serial.print(sayac);
+        sagDurum = !sagDurum;
+        Serial.print(" Sag Sinyal : ");
+        Serial.println(sagDurum);
+      } else {
+        simdikiZaman = millis();
+        fark = simdikiZaman - basmaZamani;
+        if (fark > 200) {
+          //Serial.print(" Sag Rising : ");
+          //Serial.println(sayac);
+          // Serial.print(" Sag Fark : ");
+          // Serial.println(fark);
+          sagDurum = !sagDurum;
+          basmaZamani = millis();
+
+          Serial.print(" Sag Sinyal : ");
+          Serial.println(sagDurum);
+        }
+      }
+    }
   }
 }
 void solSinyalOku() {
-  sayac++;
-  if (sayac % 2 == 0) {
-    Serial.println("solSinyal");
-    solDurum = !solDurum;
-    sayac = 0;
+  if (digitalRead(butonSolSinyal) == LOW) {
+    sayac++;
+    if (sayac % 2 == 0) {
+      sayac = 0;
+      dortluSayac++;
+      if (dortluSayac == 1) {
+        basmaZamani = millis();
+        //Serial.println(" ilk Sag Rising : ");
+        // Serial.print(sayac);
+        solDurum = !solDurum;
+        Serial.print(" Sol Sinyal : ");
+        Serial.println(solDurum);
+      } else {
+        simdikiZaman = millis();
+        fark = simdikiZaman - basmaZamani;
+        if (fark > 200) {
+
+          //Serial.println(sayac);
+          //Serial.print(" Sag Fark : ");
+          //Serial.println(fark);
+          solDurum = !solDurum;
+          Serial.print(" Sol Sinyal : ");
+          Serial.println(solDurum);
+          basmaZamani = millis();
+        }
+      }
+    }
   }
 }
+
+
+
 void dortluOku() {
-  //sayac++;
-  // if (sayac % 2 == 0) {
- boolean butonOku = digitalRead(3);
-
-    while (digitalRead(3) == HIGH) {}
-    if (digitalRead(3) == LOW) {
-     // delay(1000);
-      Serial.println("Dortlu");
-      dortluDurum = !dortluDurum;
-    }
-
-
-    // 
-
-  //}
-
-  //  sayac = 0;
-  //}
-  // if(ledDurum)
-
-}
-void farOku() {
- boolean butonOku = digitalRead(butonFar);
-
-    while (digitalRead(butonFar) == HIGH) {}
-    if (digitalRead(butonFar) == LOW) {
-    Serial.println("Far");
-    farDurum = !farDurum;
+  if (digitalRead(butonDortlu) == LOW) {
+    sayac++;
+    if (sayac % 2 == 0) {
+      sayac = 0;
+      dortluSayac++;
+      if (dortluSayac == 1) {
+        basmaZamani = millis();
+        dortluDurum = !dortluDurum;
+        Serial.print(" dortluDurum Sinyal : ");
+        Serial.println(dortluDurum);
+      } else {
+        simdikiZaman = millis();
+        fark = simdikiZaman - basmaZamani;
+        if (fark > 200) {
+          dortluDurum = !dortluDurum;
+          basmaZamani = millis();
+          Serial.print(" dortluDurum Sinyal : ");
+          Serial.println(dortluDurum);
+        }
+      }
     }
   }
-  // if(ledDurum)
+}
+
+void farOku() {
+  if (digitalRead(butonFar) == LOW) {
+    sayac++;
+    if (sayac % 2 == 0) {
+      sayac = 0;
+      dortluSayac++;
+      if (dortluSayac == 1) {
+        basmaZamani = millis();
+        farDurum = !farDurum;
+        Serial.print(" far  : ");
+        Serial.println(farDurum);
+      } else {
+        simdikiZaman = millis();
+        fark = simdikiZaman - basmaZamani;
+        if (fark > 200) {
+          farDurum = !farDurum;
+          basmaZamani = millis();
+          Serial.print(" far  : ");
+          Serial.println(farDurum);
+        }
+      }
+    }
+  }
+}
+// if(ledDurum)
 
 
 void dortluDegis(boolean dortluDurum) {
